9b97de5230ab5ce6dc3589e1706b3b28e7eae072
ruoso <ruoso@4ad37cd2-5fec-0310-835f-b3785c72a374>
2010-05-24 22:12:15 +0000

    [C-C-SOAP] compatibilizes with newer XML::Compile::SOAP which appends "Response" and expects "Response" in rpc literal return (there is no spec for that, but it seems to be a common practice)
    git-svn-id: http://dev.catalystframework.org/repos/Catalyst/Catalyst-Controller-SOAP/1.0/trunk@13314 4ad37cd2-5fec-0310-835f-b3785c72a374

--- lib/Catalyst/Controller/SOAP.pm
+++ lib/Catalyst/Controller/SOAP.pm
@@ -13,7 +13,7 @@
     use constant NS_SOAP_ENV => "http://schemas.xmlsoap.org/soap/envelope/";
     use constant NS_WSDLSOAP => "http://schemas.xmlsoap.org/wsdl/soap/";
 
-    our $VERSION = '1.21';
+    our $VERSION = '1.22';
 
     __PACKAGE__->mk_accessors qw(wsdl wsdlobj decoders encoders
          ports wsdlservice xml_compile soap_action_prefix rpc_endpoint_paths);
@@ -258,7 +258,7 @@
                 if ($operation->style eq 'rpc') {
                   $self->encoders->{$name} = sub {
                     my ($doc, $data) = @_;
-                    my $element = $doc->createElementNS($out_namespace,$name);
+                    my $element = $doc->createElementNS($out_namespace,$name.'Response');
                     $element->appendChild($_) for map {
                       $_->{compiled_writer}->($doc, $data->{$_->{name}})
                     } @{$output_parts};
